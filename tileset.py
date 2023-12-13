673bfba2a9a8b14f4eb9674ddfca19f26eb8649c
Pedro Henrique <eduardofilhojj@gmail.com>
2023-12-12 23:40:56 -0300

    estou cansado...

--- tileset.py
+++ tileset.py
@@ -3,19 +3,22 @@ from pygame.locals import *
 
 tamanho_dos_tiles = ( 96, 96 ) #(64, 64) #(96, 96)
 
+__tilemappacks = []
+plataformas = []
+
 # crie uma array
-# preencha esta array com pacotes de tileset
+# preencha esta array com pacotes de tilemap
 # cada pacote terá uma imagem e várias coordenadas
 # a imagem deverá ser exibida em cada coordenada do vetor durante o jogo
 # as coordenadas deverão ser reposicionadas de acordo com a coordenada relativa da personagem
 
-# monte um sistema de empacotamento de tileset's
-# desenvolva uma função que exiba a imagem em todas as coordenadas do tilesetpack
-# armazene os tilesetpacks de maneira compacta
+# monte um sistema de empacotamento de tilemap's
+# desenvolva uma função que exiba a imagem em todas as coordenadas do tilemappack
+# armazene os tilemappacks de maneira compacta
 
 array = []
 
-tilesetpack = (
+tilemappack_grass = (
     pygame.image.load('./tiles/chao-verde1.png'),
     [
         (-1, 0),
@@ -24,8 +27,8 @@ tilesetpack = (
         ]
     )
 
-tileset = [
-    # colunas
+tilemappack_ground = [(x, y) for x in range(-20, 20) for y in range(2, 4)]
+'''    # colunas
     (-2, 5),
     (-2, 4),
     (-2, 3),
@@ -49,42 +52,27 @@ tileset = [
     ( 7, 4),
     ( 7, 5),
 ]
-tileset.extend([
+tilemappack_ground.extend([
     # chão comprido
     (7, 6), (8, 6), (9, 6), (10, 6), (11, 6), (12, 6), (13, 6), (14, 6), (15, 6), (16, 6), (17, 6), (18, 6), (19, 6)
 ])
-tileset.extend([
+tilemappack_ground.extend([
     # sala com escada
     (-2, 7), (-2, 8), (-2, 9), (-2, 10), (-2, 11), (-1, 7), (-1, 11), (0, 7), (0, 11), (1, 7), (1, 11), (2, 7), (2, 11), (3, 7), (3, 10), (3, 11), (4, 9), (4, 10), (4, 11), (5, 8), (5, 9), (5, 10), (5, 11), (6, 7), (6, 8), (6, 9), (6, 10), (6, 11)
-])
+])'''
 
-'''for i in range(7, 20):
-    tileset.append((i, 6))
 
-for x in range(-2, 7):
-    for y in range(7, 12):
-        tileset.append((x, y))
 
-tileset.remove(( 5, 7))
-tileset.remove(( 4, 7))
-for x in range(4):
-    for y in range(3-x):
-        tileset.remove((2+x, 8+y))
-for x in range(-1, 2):
-    for y in range( 8,11):
-        tileset.remove(( x, y))'''
+tilemappack_ground.sort()
+tilemappack_ground.reverse()
 
-tileset.sort()
-tileset.reverse()
-
-plataformas = [
-]
-
-for i in tileset:
+for i in tilemappack_ground:
     plataformas.append(
         pygame.Rect( tamanho_dos_tiles[0]*i[0], tamanho_dos_tiles[1]*i[1], tamanho_dos_tiles[0], tamanho_dos_tiles[1] )
     )
 
-tileset = (pygame.image.load( "./tiles/terra-pedra_estendido.png" ), tileset)
-array.append(tileset)
-array.append(tilesetpack)
+
+tilemappack_ground = (pygame.image.load( "./tiles/terra-pedra_estendido.png" ), tilemappack_ground)
+
+array.append(tilemappack_grass)
+array.append(tilemappack_ground)
