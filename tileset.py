c7e58fae5b259b6affa93dcfa8576ddea067c84e
Pedro Henrique <eduardofilhojj@gmail.com>
2024-05-10 05:11:33 -0300

    add structural support to map editing

--- tileset.py
+++ tileset.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+from pandas import read_csv
 
 tamanho_dos_tiles = ( 96, 96 ) #(64, 64) #(96, 96)
 
@@ -23,7 +24,7 @@ array = []
 def settmpck(img_path, tileset:list):
 	return ( pygame.image.load(img_path), tileset )
 
-tilemappack_ground = []#(x, y) for x in range(-20, 20) for y in range(2, 4)]
+tilemappack_ground = []
 #tilemappack_ground.extend([(-3, 1), (-5, 1)])
 
 tilemappack_ground.extend([
@@ -53,28 +54,42 @@ tilemappack_ground.extend([
 ])
 tilemappack_ground.extend([
     # chão comprido
-    #(7, 6), (8, 6), (9, 6), (10, 6), (11, 6), (12, 6), (13, 6), (14, 6), (15, 6), (16, 6), (17, 6), (18, 6), (19, 6)
     (x, 6) for x in range(7, 20)
 ])
 tilemappack_ground.extend([
     # chão comprido inferior
-    #(7, 6), (8, 6), (9, 6), (10, 6), (11, 6), (12, 6), (13, 6), (14, 6), (15, 6), (16, 6), (17, 6), (18, 6), (19, 6)
     (x, 7) for x in range(7, 20)
 ])
 tilemappack_ground.extend([
     # sala com escada
     (-2, 7), (-2, 8), (-2, 9), (-2, 10), (-2, 11), (-1, 7), (-1, 11), (0, 7), (0, 11), (1, 7), (1, 11), (2, 7), (2, 11), (3, 10), (3, 11), (4, 9), (4, 10), (4, 11), (5, 8), (5, 9), (5, 10), (5, 11), (6, 7), (6, 8), (6, 9), (6, 10), (6, 11)
 ])
+tilemappack_ground.extend([(-1,1)])
 
 
 
 tilemappack_ground.sort()
 tilemappack_ground.reverse()
 
-for i in tilemappack_ground:
-    plataformas.append(
-        pygame.Rect( tamanho_dos_tiles[0]*i[0], tamanho_dos_tiles[1]*i[1], tamanho_dos_tiles[0], tamanho_dos_tiles[1] )
-    )
+ff = read_csv('./maps/map.csv').to_numpy()
+
+for i in range(len(ff)):
+	ff[i] = list(ff[i])
+
+tilemappack_ground = []
+
+for i in range(len(ff)):
+	for j in range(len(ff[i])):
+		if ff[i][j]:
+			tilemappack_ground.append((j, i))
+			plataformas.append(
+				pygame.Rect( tamanho_dos_tiles[0]*j, tamanho_dos_tiles[1]*i, tamanho_dos_tiles[0], tamanho_dos_tiles[1] )
+			)
+
+#for i in tilemappack_ground:
+#	plataformas.append(
+#		pygame.Rect( tamanho_dos_tiles[0]*i[0], tamanho_dos_tiles[1]*i[1], tamanho_dos_tiles[0], tamanho_dos_tiles[1] )
+#	)
 
 
 tilemappack_ground = (pygame.image.load( "./tiles/terra-pedra_estendido.png" ), tilemappack_ground)
